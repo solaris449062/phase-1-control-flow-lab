6dbabc7a4e45f016bef69b6ee6d60835270070d0
Sungoh Park <spark449062@gmail.com>
2021-07-20 01:44:37 -0600

    commit tree 30bc5b46950263714be8367f7ef90452bc094011

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) {
+    return "This one is on me!";
+  }
+  else if (depth >= 2000 && depth < 2500 ) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else if (depth >= 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let statement;
+  switch (tip) {
+    case "generous":
+      statement = "Thank you so much."
+      break;
+    case "not as generous":
+      statement = "Thank you."
+      break;
+    default: 
+      statement = "Bye."
+  }
+  return statement;
 }
